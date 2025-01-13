d71869d05a80d3a3af4b323bc8a200f270ddc63c
Mario de Domingo <wolverine.mda.307@gmail.com>
2025-01-13 12:49:17 +0100

    añado el modelo cuenta

---
+++ Vives_Bank_Net/Vives_Bank_Net/Rest/Cliente/Models/Direccion.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Vives_Banks_Net.Rest.Cliente;
+
+public class Direccion
+{
+    [Required(ErrorMessage = "La calle no puede estar vacia")]
+    public string Calle { get; set; }
+
+    [Required(ErrorMessage = "El número no puede estar vacio")]
+    public string Numero { get; set; }
+
+    [Required(ErrorMessage = "El código postal no puede estar vacío")]
+    [RegularExpression(@"^\d{5}$", ErrorMessage = "El código postal debe tener 5 números")]
+    public string CodigoPostal { get; set; }
+
+    [Required(ErrorMessage = "El piso no puede estar vacio")]
+    public string Piso { get; set; }
+
+    [Required(ErrorMessage = "La letra no puede estar vacia")]
+    public string Letra { get; set; }
+}
---
+++ Vives_Bank_Net/Vives_Bank_Net/Rest/Cuenta/Models/Cuenta.cs
@@ -0,0 +1,6 @@
+﻿namespace Vives_Bank_Net.Rest.Cuenta.Models;
+
+public class Cuenta
+{
+    
+}
