e7bdcacaf24f8d5848b714a2fb9ea1018665124f
Jaimelen10 <jleonmulero@gmail.com>
2025-01-14 18:01:23 +0100

    Añadidos type schema y query de movimientos con graphql

---
+++ Vives_Bank_Net/Vives_Bank_Net/GraphQL/MovimientoQuery.cs
@@ -0,0 +1,13 @@
+﻿
+using GraphQL.Types;
+
+namespace Vives_Bank_Net.GraphQL;
+
+public class MovimientoQuery : ObjectGraphType
+{
+    public MovimientoQuery()
+    {
+        Field<ListGraphType<MovimientoType>>("movimientos")
+            .Resolve(context => null); // TODO: CAMBIAR NULL POR GETALL DE MOVIMIENTO
+    }
+}
