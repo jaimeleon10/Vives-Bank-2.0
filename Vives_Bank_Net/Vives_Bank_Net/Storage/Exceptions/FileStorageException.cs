5cca5279abe0776b998bfc6425cbea5de0b16015
Mario de Domingo <wolverine.mda.307@gmail.com>
2025-01-14 16:01:42 +0100

    Creo las escepciones del storage

---
+++ Vives_Bank_Net/Vives_Bank_Net/Storage/Exceptions/FileStorageException.cs
@@ -0,0 +1,12 @@
+﻿namespace Vives_Bank_Net.Storage.Exceptions;
+
+public class FileStorageException : Exception
+{
+    public FileStorageException(string message) : base(message)
+    {
+    }
+
+    public FileStorageException(string message, Exception innerException) : base(message, innerException)
+    {
+    }
+}
