0b3377ecafa3bc1064c95abe885a92f696bbfc0f
Natalia González Álvarez <nagonal2004@gmail.com>
2025-01-15 12:33:37 +0100

    hechos tests validos de storageJson
    hechos los test de los casos validos de storageJson

--- Vives_Bank_Net/Vives_Bank_Net/Test/StorageJsonTest.cs
+++ Vives_Bank_Net/Vives_Bank_Net/Test/StorageJsonTest.cs
@@ -1,5 +1,7 @@
 ﻿using Moq;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Serialization;
 using NUnit.Framework;
 using Vives_Banks_Net.Rest.User;
 
@@ -15,7 +17,13 @@ public class StorageJsonTest
     public StorageJsonTest()
     {
         _logger = new Mock<ILogger<StorageJson>>();
-        _jsonSettings = new JsonSerializerSettings();
+        _jsonSettings = new JsonSerializerSettings
+        {
+            Formatting = Formatting.Indented,
+            Converters = { new StringEnumConverter() },
+            ContractResolver = new CamelCasePropertyNamesContractResolver(),
+            DateFormatHandling = DateFormatHandling.IsoDateFormat
+        };
         _storageJson = new StorageJson(_logger.Object);
     }
 
@@ -24,18 +32,54 @@ public class StorageJsonTest
     {
         var path = Path.Combine(Path.GetTempPath(), "testImport.json");
         var file = new FileInfo(path);
-        var data1 = new User{Id = 1L, Guid = "guid", UserName = "username", PasswordHash = "password", Role = Role.USER, CreatedAt = DateTime.Parse("2025-01-14 10:30:00"), UpdatedAt = DateTime.Parse("2025-01-14 10:30:00"), IsDeleted = false};
-        var data2 = new User{Id = 2L, Guid = "guid2", UserName = "username2", PasswordHash = "password2", Role = Role.USER, CreatedAt = DateTime.Parse("2025-01-13 10:30:00"), UpdatedAt = DateTime.Parse("2025-01-13 10:30:00"), IsDeleted = false};
-        var data = new List<User>{data1, data2};
-        
+
+        var data1 = new User
+        {
+            Id = 1L,
+            Guid = "guid",
+            UserName = "username",
+            PasswordHash = "password",
+            Role = Role.USER,
+            CreatedAt = DateTime.Parse("2025-01-14 10:30:00"),
+            UpdatedAt = DateTime.Parse("2025-01-14 10:30:00"),
+            IsDeleted = false
+        };
+
+        var data2 = new User
+        {
+            Id = 2L,
+            Guid = "guid2",
+            UserName = "username2",
+            PasswordHash = "password2",
+            Role = Role.USER,
+            CreatedAt = DateTime.Parse("2025-01-13 10:30:00"),
+            UpdatedAt = DateTime.Parse("2025-01-13 10:30:00"),
+            IsDeleted = false
+        };
+
+        var data = new List<User> { data1, data2 };
+
         _storageJson.ExportJson(file, data);
-        _storageJson.ImportJson<List<User>>(file);
+
+        Assert.That(File.Exists(file.FullName), Is.True);
         
-        var content = File.ReadAllText(path);
-        var expectedJson = JsonConvert.DeserializeObject(data.ToString(), _jsonSettings);
+        var content = File.ReadAllText(file.FullName);
+        Assert.That(!string.IsNullOrEmpty(content), Is.True);
         
-        Assert.That(content.Replace("\r\n", "").Replace("\n", "").Replace(" ", ""), Is.EqualTo(expectedJson));
-        Assert.That(content.Replace("\r\n", "").Replace("\n", "").Replace(" ", ""), Is.Not.Null);
+        var contentDeserializado = JsonConvert.DeserializeObject<List<User>>(content, _jsonSettings);
+        
+        Assert.That(contentDeserializado.Count, Is.EqualTo(data.Count));
+        for (int i = 0; i < data.Count; i++)
+        {
+            Assert.That(contentDeserializado[i].Id, Is.EqualTo(data[i].Id));
+            Assert.That(contentDeserializado[i].Guid, Is.EqualTo(data[i].Guid));
+            Assert.That(contentDeserializado[i].UserName, Is.EqualTo(data[i].UserName));
+            Assert.That(contentDeserializado[i].PasswordHash, Is.EqualTo(data[i].PasswordHash));
+            Assert.That(contentDeserializado[i].Role, Is.EqualTo(data[i].Role));
+            Assert.That(contentDeserializado[i].CreatedAt, Is.EqualTo(data[i].CreatedAt));
+            Assert.That(contentDeserializado[i].UpdatedAt, Is.EqualTo(data[i].UpdatedAt));
+            Assert.That(contentDeserializado[i].IsDeleted, Is.EqualTo(data[i].IsDeleted));
+        }
     }
     
     [Test]
@@ -43,8 +87,31 @@ public class StorageJsonTest
     {
         var path = Path.Combine(Path.GetTempPath(), "testExport.json");
         var file = new FileInfo(path);
-        var data1 = new User{Id = 1L, Guid = "guid", UserName = "username", PasswordHash = "password", Role = Role.USER, CreatedAt = DateTime.Parse("2025-01-14 10:30:00"), UpdatedAt = DateTime.Parse("2025-01-14 10:30:00"), IsDeleted = false};
-        var data2 = new User{Id = 2L, Guid = "guid2", UserName = "username2", PasswordHash = "password2", Role = Role.USER, CreatedAt = DateTime.Parse("2025-01-13 10:30:00"), UpdatedAt = DateTime.Parse("2025-01-13 10:30:00"), IsDeleted = false};
+        
+        var data1 = new User
+        {
+            Id = 1L,
+            Guid = "guid",
+            UserName = "username",
+            PasswordHash = "password",
+            Role = Role.USER,
+            CreatedAt = DateTime.Parse("2025-01-14 10:30:00"),
+            UpdatedAt = DateTime.Parse("2025-01-14 10:30:00"),
+            IsDeleted = false
+        };
+
+        var data2 = new User
+        {
+            Id = 2L,
+            Guid = "guid2",
+            UserName = "username2",
+            PasswordHash = "password2",
+            Role = Role.USER,
+            CreatedAt = DateTime.Parse("2025-01-13 10:30:00"),
+            UpdatedAt = DateTime.Parse("2025-01-13 10:30:00"),
+            IsDeleted = false
+        };
+        
         var data = new List<User>{data1, data2};
         
         _storageJson.ExportJson(file, data);
@@ -53,9 +120,8 @@ public class StorageJsonTest
         
         var content = File.ReadAllText(path);
         var expectedJson = JsonConvert.SerializeObject(data, _jsonSettings);
-        expectedJson.Replace("\r\n", "").Replace("\n", "").Replace(" ", "");
         
-        Assert.That(content.Replace("\r\n", "").Replace("\n", "").Replace(" ", ""), Is.EqualTo(expectedJson));
+        Assert.That(content, Is.EqualTo(expectedJson));
         
         File.Delete(path);
     }
