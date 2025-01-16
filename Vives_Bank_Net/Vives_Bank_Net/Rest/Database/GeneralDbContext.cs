fb314099fdb6957ff05bb266b3025097c2668a69
Jaimelen10 <jleonmulero@gmail.com>
2025-01-17 00:00:25 +0100

    Modificado generalDbContext añadiendo todas las entidades
    Borrado dbContext individuales no necesarios

--- Vives_Bank_Net/Vives_Bank_Net/Rest/Database/GeneralDbContext.cs
+++ Vives_Bank_Net/Vives_Bank_Net/Rest/Database/GeneralDbContext.cs
@@ -29,5 +29,64 @@ public class GeneralDbContext : DbContext
         modelBuilder.Entity<BaseEntity>().ToTable("ProductosBase");
         modelBuilder.Entity<CuentaEntity>().ToTable("Cuentas");
         modelBuilder.Entity<TarjetaEntity>().ToTable("Tarjetas");
+
+        // User Entity
+        modelBuilder.Entity<UserEntity>(entity =>
+        {
+            entity.Property(e => e.CreatedAt).IsRequired().ValueGeneratedOnAdd();
+            entity.Property(e => e.UpdatedAt).IsRequired().ValueGeneratedOnAddOrUpdate(); // Asegura la actualización
+        });
+
+        // Cliente Entity
+        modelBuilder.Entity<ClienteEntity>(entity =>
+        {
+            entity.Property(ent => ent.CreatedAt).IsRequired().ValueGeneratedOnAdd();
+            entity.Property(ent => ent.UpdatedAt).IsRequired().ValueGeneratedOnUpdate();
+            entity.OwnsOne(e => e.Direccion);
+            // TODO: Revisar relacidón Cliente - Usuario
+        });
+        
+        // Base Entity
+        modelBuilder.Entity<BaseEntity>(entity =>
+        {
+            entity.Property(e => e.CreatedAt).IsRequired().ValueGeneratedOnAdd();
+            entity.Property(e => e.UpdatedAt).IsRequired().ValueGeneratedOnUpdate();
+            // TODO: Revisar si hay que añadir relación
+        });
+        
+        // Cuenta Entity
+        // TODO: Revisar todas las relaciones
+        modelBuilder.Entity<CuentaEntity>(entity =>
+        {
+            entity.Property(e => e.CreatedAt).IsRequired().ValueGeneratedOnAdd();
+            entity.Property(e => e.UpdatedAt).IsRequired().ValueGeneratedOnUpdate();
+        });
+        
+        modelBuilder.Entity<CuentaEntity>()
+            .HasOne(c => c.Cliente) 
+            .WithMany() 
+            .HasForeignKey(c => c.ClienteId) 
+            .IsRequired(); 
+        
+        /*
+         modelBuilder.Entity<CuentaEntity>()
+            .HasOne(c => c.TarjetaId)
+            .WithOne()
+            .HasForeignKey(c => c.TarjetaId)
+            .IsRequired(false);
+        */
+        
+        modelBuilder.Entity<CuentaEntity>()
+            .HasOne(c => c.Producto) 
+            .WithMany() 
+            .HasForeignKey(c => c.ProductoId)
+            .IsRequired(); 
+        
+        // Tarjeta Entity
+        modelBuilder.Entity<TarjetaEntity>(entity =>
+        {
+            entity.Property(e => e.CreatedAt).IsRequired().ValueGeneratedOnAdd();
+            entity.Property(e => e.UpdatedAt).IsRequired().ValueGeneratedOnUpdate();
+        });
     }
 }
