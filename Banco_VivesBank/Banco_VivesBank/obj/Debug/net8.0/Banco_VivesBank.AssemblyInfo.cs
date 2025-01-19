32dbf02a897c247b074669dd978b50cfb2b40635
Mario de Domingo Alvarez <wolverine.mda.307@gmail.com>
2025-01-19 23:08:42 +0100

    restaurando algunos archivos

--- Banco_VivesBank/Banco_VivesBank/obj/Debug/net8.0/Banco_VivesBank.AssemblyInfo.cs
+++ Banco_VivesBank/Banco_VivesBank/obj/Debug/net8.0/Banco_VivesBank.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Banco_VivesBank")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+063a1908eb5e0660c60f60174275cfa5e13fcd1e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1833824da38112d3ffe9a47bbd3c0373f1d9fa0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Banco_VivesBank")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Banco_VivesBank")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
