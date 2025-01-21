fdea7a58ceddd0d29dd689e64c7b72cbf54fd8ec
yahya el hadri <yahyaelhadricgs@gmail.com>
2025-01-21 15:29:19 +0100

    Cambios cuentaservice

--- Banco_VivesBank/Banco_VivesBank/Producto/Cuenta/Dto/CuentaUpdateRequest.cs
+++ Banco_VivesBank/Banco_VivesBank/Producto/Cuenta/Dto/CuentaUpdateRequest.cs
@@ -1,6 +1,12 @@
-﻿namespace Banco_VivesBank.Producto.Cuenta.Dto;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Banco_VivesBank.Producto.Cuenta.Dto;
 
 public class CuentaUpdateRequest
 {
+    [Required(ErrorMessage = "El campo Saldo es obligatorio")]
     public string Saldo { get; set; }
+    
+    [Required(ErrorMessage = "El campo clienteId es obligatorio")]
+    public string ClienteGuid { get; set; }
 }
