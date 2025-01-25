c000965c12bc6babc422e6ee0456c3ad4fb7ebd0
Mario de Domingo <wolverine.mda.307@gmail.com>
2025-01-25 14:35:38 +0100

    configurando ftp y moviendo archivos

--- Banco_VivesBank/Banco_VivesBank/Storage/Files/Exceptions/FileStorageNotFoundException.cs
+++
@@ -1,8 +0,0 @@
-namespace Banco_VivesBank.Storage.Files.Exceptions;
-
-public class FileStorageNotFoundException : FileStorageException
-{
-    public FileStorageNotFoundException(string message) : base(message)
-    {
-    }
-}
---
+++ Banco_VivesBank/Banco_VivesBank/Storage/Ftp/Exceptions/FtpException.cs
@@ -0,0 +1,6 @@
+﻿namespace Banco_VivesBank.Storage.Ftp.Exceptions;
+
+public class FtpException : Exception
+{
+    public FtpException(string mensaje) : base(mensaje) { }
+}
