e8b450f90e73cf91ff2a10105b779dee3575c556
Mario de Domingo <wolverine.mda.307@gmail.com>
2025-02-02 17:33:06 +0100

    documentado el storage de imagenes

--- Banco_VivesBank/Banco_VivesBank/Storage/Images/Exceptions/FileStorageException.cs
+++ Banco_VivesBank/Banco_VivesBank/Storage/Images/Exceptions/FileStorageException.cs
@@ -1,8 +1,13 @@
 ﻿namespace Banco_VivesBank.Storage.Images.Exceptions;
 
+/// <summary>
+/// Excepción personalizada para errores en el almacenamiento de archivos.
+/// </summary>
 public class FileStorageException : Exception
 {
-    public FileStorageException(string message) : base(message)
-    {
-    }
+    /// <summary>
+    /// Constructor de la excepción de almacenamiento de archivos.
+    /// </summary>
+    /// <param name="message">Mensaje de error.</param>
+    public FileStorageException(string message) : base(message) { }
 }
--- Banco_VivesBank/Banco_VivesBank/Storage/Images/Exceptions/FileStorageNotFoundException.cs
+++ Banco_VivesBank/Banco_VivesBank/Storage/Images/Exceptions/FileStorageNotFoundException.cs
@@ -1,8 +1,13 @@
 namespace Banco_VivesBank.Storage.Images.Exceptions;
 
+/// <summary>
+/// Excepción lanzada cuando no se encuentra un archivo en el almacenamiento.
+/// </summary>
 public class FileStorageNotFoundException : FileStorageException
 {
-    public FileStorageNotFoundException(string message) : base(message)
-    {
-    }
+    /// <summary>
+    /// Constructor de la excepción cuando un archivo no se encuentra.
+    /// </summary>
+    /// <param name="message">Mensaje de error.</param>
+    public FileStorageNotFoundException(string message) : base(message) { }
 }
