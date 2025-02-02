d28a0ac2a6d646605351f799e57def6fcc668048
Mario de Domingo <wolverine.mda.307@gmail.com>
2025-02-02 17:18:32 +0100

    documentado storageCsv de productos

--- Banco_VivesBank/Banco_VivesBank/Producto/ProductoBase/Storage/StorageProductos.cs
+++ Banco_VivesBank/Banco_VivesBank/Producto/ProductoBase/Storage/StorageProductos.cs
@@ -1,17 +1,28 @@
 ﻿using System.Globalization;
 using System.Text;
-using Banco_VivesBank.Producto.Base.Storage;
 
 namespace Banco_VivesBank.Producto.ProductoBase.Storage;
+/// <summary>
+/// Clase encargada del almacenamiento y procesamiento de productos mediante archivos CSV.
+/// </summary>
 public class StorageProductos : IStorageProductos
 {
     private readonly ILogger<StorageProductos> _logger;
 
+    /// <summary>
+    /// Constructor de la clase StorageProductos.
+    /// </summary>
+    /// <param name="logger">Logger para el registro de eventos.</param>
     public StorageProductos(ILogger<StorageProductos> logger)
     {
         _logger = logger;
     }
 
+    /// <summary>
+    /// Importa productos desde un archivo CSV.
+    /// </summary>
+    /// <param name="file">Objeto FileInfo que representa el archivo CSV.</param>
+    /// <returns>Lista de productos importados.</returns>
     public List<ProductoBase.Models.Producto> ImportProductosFromCsv(FileInfo file)
     {
         _logger.LogDebug($"Importando productos desde CSV de {nameof(ProductoBase.Models.Producto)}");
@@ -48,6 +59,11 @@ public class StorageProductos : IStorageProductos
         }
     }
     
+    /// <summary>
+    /// Exporta una lista de productos a un archivo CSV.
+    /// </summary>
+    /// <param name="file">Objeto FileInfo que representa el archivo CSV de destino.</param>
+    /// <param name="data">Lista de productos a exportar.</param>
     public void ExportProductosFromCsv(FileInfo file, List<ProductoBase.Models.Producto> data)
     {
         _logger.LogDebug($"Exportando productos a CSV de {nameof(ProductoBase.Models.Producto)}");
